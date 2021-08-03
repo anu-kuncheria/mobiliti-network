e09f277515d38faca6c698702a01456ac2e8d2f7
Anu Kuncheria <akuncheria@ses7616872.local>
2021-08-03 16:51:04 -0700

    shapenodes script updated

--- s3_length_attribute.py
+++ s3_length_attribute.py
@@ -8,8 +8,7 @@ import ast
 
 '''
 # Length is to be calculated from shapenodes. But shapenodes for B,F,T links
-
-sn = pd.read_csv(r'C:\Users\anuku\GSR\UCBerkeley_GSR\Networks_Dataset\from_Here\nav_street_for_mobiliti_links_shapenodes.csv', names = ['global_id','LINK_ID','nodes'])
+sn = pd.read_csv('nav_street_for_mobiliti_links_shapenodes.csv', names = ['global_id','LINK_ID','nodes'])
 sn.head(5)  # link id and shape nodes for each link
 #create additional links in shape nodes column
 sn_bi = sn[sn['LINK_ID'].isin(b1['LINK_ID'].values)] #bidirec
@@ -20,10 +19,10 @@ sn_FT = sn[~sn['LINK_ID'].isin(b1['LINK_ID'].values)]  #unidirec FT links
 shape_nodes_unidirec = sn_FT.append([s2,sn_bi])
 len(shape_nodes_unidirec)
 shape_nodes_unidirec = shape_nodes_unidirec.drop('global_id', axis = 1)
-#shape_nodes_unidirec.to_csv(r'C:\Users\anuku\GSR\UCBerkeley_GSR\Networks_Dataset\networks_dataset_Mobiliti\Nov2019\shapenodes_unidirec.csv', index = False)
+#shape_nodes_unidirec.to_csv('\networks_dataset_Mobiliti\Nov2019\shapenodes_unidirec.csv', index = False)
 '''
 
-shape_nodes = pd.read_csv(r'C:\Users\anuku\GSR\UCBerkeley_GSR\Networks_Dataset\networks_dataset_Mobiliti\Nov2019\midstages\shapenodes_unidirec.csv')
+shape_nodes = pd.read_csv('\networks_dataset_Mobiliti\Nov2019\midstages\shapenodes_unidirec.csv')
 from math import radians, cos, sin, asin, sqrt
 def haversine(lon1, lat1, lon2, lat2):
     """
@@ -56,11 +55,10 @@ shape_nodes.to_csv("../midstages/shapenodes_linklength.csv", index = False)
 # adding link length to all links from the shapenodes calcuated
 all_links_ca_uni  = pd.read_csv("all_links_ca_uni_nolen.csv")
 all_links_ca_uni_length = pd.merge(all_links_ca_uni,shape_nodes, on= 'LINK_ID')
-
 #CHECK-  all links with same PID has same links length
 all_links_ca_uni_length.groupby(['PID','LENGTH(meters)'])['LINK_ID'].count().value_counts()
 
-#FILTER FOR FINAL SAVE
+#Filter for final save
 all_links_ca_uni_length_f = all_links_ca_uni_length[['LINK_ID','ST_NAME','REF_IN_ID','NREF_IN_ID','FUNC_CLASS','DIR_TRAVEL','NUM_PHYS_LANES','SPEED_KPH','CAPACITY','LENGTH(meters)','N_SHAPEPNT']]
 all_links_ca_uni_length_f.rename(columns = {'CAPACITY':'CAPACITY(veh/hour)'}, inplace = True)
 all_links_ca_uni_length_f.to_csv('\Nov2019\all_links_ca.csv', index = False)
