7acb85dcf22468f2c850648103df616fc499d122
Anu Kuncheria <akuncheria@ses7616872.local>
2022-08-26 12:01:46 -0700

    column name changed

--- sacog/sacog_popdensity.py
+++ sacog/sacog_popdensity.py
@@ -7,7 +7,10 @@ print(len(popdensity_pre))
 print(popdensity_pre.columns)
 
 popdensity = popdensity_pre[['NODE_ID', 'RASTERVALU']]
-popdensity.rename(columns = {'RASTERVALU':'pop_density'}, inplace = True)
+popdensity.rename(columns = {'RASTERVALU':'popdensity'}, inplace = True)
+print('*****', len(popdensity[popdensity['popdensity']<0]))
+popdensity['popdensity'] = popdensity['popdensity'].replace([-9999.], 0)
+print('***', len(popdensity[popdensity['popdensity']<0]))
 print(popdensity.isnull().sum())
 print(len(popdensity))
 popdensity.to_csv('../../sacog_region_data/final/sacog_popdensity.csv', index = False)
